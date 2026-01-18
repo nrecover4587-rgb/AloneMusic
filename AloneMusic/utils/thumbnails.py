7c885fa18ff20fb80ee487908d43a4ee20081a5e
nrecover4587-rgb <nrecover4587@gmail.com>
2026-01-19 04:04:25 +0530

    Update thumbnails.py

--- AloneMusic/utils/thumbnails.py
+++ AloneMusic/utils/thumbnails.py
@@ -38,9 +38,9 @@ CACHE_DIR.mkdir(exist_ok=True)
 
 CANVAS_W, CANVAS_H = 1320, 760
 
-FONT_REGULAR_PATH = "ShrutiMusic/assets/font2.ttf"
-FONT_BOLD_PATH = "ShrutiMusic/assets/font3.ttf"
-DEFAULT_THUMB = "ShrutiMusic/assets/ShrutiBots.jpg"
+FONT_REGULAR_PATH = "AloneMusic/assets/font2.ttf"
+FONT_BOLD_PATH = "AloneMusic/assets/font3.ttf"
+DEFAULT_THUMB = "AloneMusic/assets/AloneMusicBots.jpg"
 
 
 def wrap_text(draw, text, font, max_width):
